1aa48fa8ff876d5d5f9af71a91e6a6eca1762da5
Mathieu Wingel <mathieu.wingel@sihm.fr>
2016-12-13 16:39:35 +0100

    ajout hello world

---
+++ PycharmProjects/getting_started/hello_world.py
@@ -0,0 +1,22 @@
+print("hello world")
+
+# tutorial de base : https://openclassrooms.com/courses/apprenez-a-programmer-en-python/le-monde-merveilleux-des-variables
+
+# triple double quote pour isoler un string avec des caractères ' ou "
+chaine = """salut les copains !
+comment
+va t'il ?"""
+
+a = 5
+b = 32
+
+# inverse les valeurs de a et de b
+a, b = b, a
+
+x = a = b
+
+# backslash pour dire que la commande n'est pas terminée
+p = 1 + 2 - 6 *\
+45 + 2
+
+
