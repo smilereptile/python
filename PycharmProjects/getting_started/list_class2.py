067c1fa067aa9107f6474d38dc6557f32c117be6
Mathieu Wingel <mathieu.wingel@sihm.fr>
2017-01-06 11:07:58 +0100

    adding kace program

---
+++ PycharmProjects/getting_started/list_class2.py
@@ -0,0 +1,13 @@
+# -*-coding:Latin-1 -*
+
+ma_chaine = "Bonjour à tous"
+list_from_string = ma_chaine.split()
+
+print(ma_chaine, list_from_string)
+
+ma_liste = ['Bonjour', 'à', 'tous']
+string_from_list = " ".join(ma_liste)
+
+print(ma_liste, string_from_list)
+
+def afficher_flottant(flottant):
