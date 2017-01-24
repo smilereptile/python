397ad1c597506109f0e062082a2abe6fcb585bf9
Mathieu Wingel <mathieu.wingel@sihm.fr>
2017-01-24 10:27:30 +0100

    adding classes studies

---
+++ PycharmProjects/getting_started/classes/class.py
@@ -0,0 +1,15 @@
+class Personne:
+    """Classe qui définit une personne:
+    nom :
+    prenom :
+    age :
+    residence"""
+
+
+    def __init__(self):
+        """Constructeur. On donne une valeur par défaut"""
+
+        self.nom = "Mouzeau"
+        self.prenom = "Papi"
+        self.age = 75
+        self.residence = "Paris"
